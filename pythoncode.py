6c945da37c024b59a3630faaf8d1b1634b2d5d14
DevYANahid <codealgorithmschool@gmail.com>
2024-08-16 17:55:09 +0600

    if else

--- pythoncode.py
+++ pythoncode.py
@@ -1,1 +1,10 @@
-print ("py first code")
+a = int(input("Enter Your First Number : ")); 
+b = int(input("Enter Your Second Number : ")); 
+c = int(input("Enter Your Third Number : ")); 
+
+if (a>=b and a>=c):
+    print("first Number is largest");
+elif (b>=c):
+    print("second Number is largest");
+else :
+    print("third Number is largest"); 
